32e524240692fbff3ec0a545592e3ac2e20ab67a
彷徨 <834207172@qq.com>
2020-11-30 11:20:06 +0800

    项目创建

---
+++ Assets/New/Breah.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breah : MonoBehaviour
+{
+    private MeshRenderer MeshRenderer;
+
+    private void Start()
+    {
+        MeshRenderer = this.GetComponent<MeshRenderer>();
+    }
+
+
+    private void OnMouseEnter()
+    {
+        Debug.Log(111);
+        MeshRenderer.material.SetFloat("_isture", 1.0f);
+    }
+    private void OnMouseExit()
+    {
+        Debug.Log(222);
+        MeshRenderer.material.SetFloat("_isture", 0f);
+    }
+
+
+}
